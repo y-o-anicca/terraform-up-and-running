edbda2ad30d299e32a0445f3ee9217d169d3b876
y-o-anicca <yodog712@gmail.com>
2022-07-23 17:42:37 +0900

    add null resource example

---
+++ null-resouce/main.tf
@@ -0,0 +1,11 @@
+resource "null_resource" "example" {
+  # Use UUID to force this null_resource to be recreated on every
+  # call to 'terraform apply'
+  triggers = {
+    uuid = uuid()
+  }
+
+  provisioner "local-exec" {
+    command = "echo \"Hello, World from $(uname -smp)\""
+  }
+}
